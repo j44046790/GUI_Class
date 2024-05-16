1c7d65b40a16d13043a34bfad41c49b066d9fdd4
j44046790 <j44046790@gmail.com>
2024-05-16 21:10:31 +0800

    game

--- N11320017_GUI_Class/N11320017_GUI_Class/FormGameFinalPaseword.Designer.cs
+++ N11320017_GUI_Class/N11320017_GUI_Class/FormGameFinalPaseword.Designer.cs
@@ -52,19 +52,19 @@
             this.label1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label1.Location = new System.Drawing.Point(57, 74);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(162, 24);
+            this.label1.Size = new System.Drawing.Size(200, 24);
             this.label1.TabIndex = 1;
-            this.label1.Text = "按開始按鈕初始化";
+            this.label1.Text = "按開始按鈕初始化變數";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(55, 111);
+            this.label2.Location = new System.Drawing.Point(57, 110);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(162, 24);
+            this.label2.Size = new System.Drawing.Size(200, 24);
             this.label2.TabIndex = 2;
-            this.label2.Text = "按開始按鈕初始化";
+            this.label2.Text = "按開始按鈕初始化變數";
             // 
             // textBox1
             // 
@@ -82,6 +82,7 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "猜!";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // FormGameFinalPaseword
             // 
